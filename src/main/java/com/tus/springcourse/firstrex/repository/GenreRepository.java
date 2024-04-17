691162132e91a924e16b63e079acb72c94744988
Blados <v.tusyuk@bk.ru>
2024-04-17 23:52:03 +0200

    +method which is checking exiting of element what we want to add in our list

--- src/main/java/com/tus/springcourse/firstrex/repository/GenreRepository.java
+++ src/main/java/com/tus/springcourse/firstrex/repository/GenreRepository.java
@@ -11,11 +11,16 @@ import java.util.List;
 public class GenreRepository {
     private List<Genre> genreList = new ArrayList<>();
 
+
     public GenreRepository() {
         Genre genre1 = Genre.builder().id(1).name("Drama").build();
         Genre genre2 = Genre.builder().id(2).name("Action").build();
         genreList.add(genre1);
         genreList.add(genre2);
+    }
+
+    public List<Genre> getGenreList() {
+        return genreList;
     }
 
     // Метод на добавление
--- src/main/java/com/tus/springcourse/firstrex/service/GenreService.java
+++ src/main/java/com/tus/springcourse/firstrex/service/GenreService.java
@@ -4,9 +4,7 @@ import com.tus.springcourse.firstrex.model.Genre;
 import com.tus.springcourse.firstrex.repository.GenreRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.*;
 
-import java.util.ArrayList;
 import java.util.List;
 
 @Service
@@ -15,9 +13,16 @@ public class GenreService {
     @Autowired
     private GenreRepository genreRepository;
 
-    public Genre addGenre(Genre genre) {
-        genreRepository.addGenre(genre);
 
+
+    public Genre addGenre(Genre genre) {
+        List<Genre> listCheck = genreRepository.getGenreList();
+        if(!listCheck.contains(genre)){
+            genreRepository.addGenre(genre);
+        } else {
+            System.out.println("соси бибу");
+        }
+        return genre;
     }
 
 }
